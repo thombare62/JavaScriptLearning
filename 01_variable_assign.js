9dc53a96e410b6127006a57759401991b6ecb118
thombare62 <thombare62@gmail.com>
2022-12-08 21:04:39 +0530

    Assignment 01
    this task includes declaration  initialisation  and console logging process

---
+++ 01_variable_assign.js
@@ -0,0 +1,15 @@
+var myFullName; //
+myFullName = "Chandrashekhar Thombare" //
+console.log(myFullName);
+var myFullName = "Chandrashekhar Bharat Thombare" //
+console.log(myFullName);
+var myLove = "I love only JS" //
+console.log(myLove)
+var firstName = "Chandrashekhar" //
+var lastName = "Thombare" //
+var age = "25" //
+var address = "At Masalwadi, Post LoniBhapkar, Taluka Baramati, District Pune." //
+var pin_code = "400709" //
+var gender = "Male" //
+var is_married = "No" //
+console.log(firstName, lastName, age, address, pin_code, gender, is_married)
