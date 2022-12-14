b37bfadbf4694e45a60da435539d469b2645a93a
thombare62 <thombare62@gmail.com>
2022-12-14 22:52:35 +0530

    Create 03_stringTemplateAssign.js

---
+++ 03_stringTemplateAssign.js
@@ -0,0 +1,25 @@
+function squareOfWordLength(value1, value2,value3) {
+    var wordLength = value1.length;
+   var result = wordLength*wordLength;
+   console.log("length of word JavaScript is =",wordLength ,"And its square is =", result);
+  
+   var wordLength = value2.length;
+   var result = wordLength*wordLength;
+   console.log("length of word Google Chrome is =",wordLength ,"And its square is =", result);
+  
+   var wordLength = value3.length;
+   var result = wordLength*wordLength;
+   console.log("length of word Developer Smart is =",wordLength ,"And its square is =", result);
+     
+  } 
+
+  squareOfWordLength ("JavaScript","Google Chrome","Developer Smart");
+  var developer = "I am Angular Developer";
+  var devLength = developer.length;
+  var word = developer.split(" ");
+  console.log("Total length in String is =",devLength);
+  console.log(" Total No of Word is =",word);
+  console.log("Divided by length and number of Words =",devLength/4);
+  console.log("Multiply by length and number of Words =",devLength*4);
+  
+  
