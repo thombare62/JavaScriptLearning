e89b7a008aad362ebc8c3a800ac2a35281d985af
thombare62 <thombare62@gmail.com>
2022-12-22 21:05:32 +0530

    Eg
    Eg

--- egForFridaysAssign.js
+++ egForFridaysAssign.js
@@ -1,15 +1,3 @@
-var votingEligibility = function(age){
-    if(age<=0 || age>120 ){
-        console.log(`Age value : ${age}, Invalid data`);
-    }
-    if(age>=18 && age<=120) {
-        console.log(`your age is : ${age}, You can vote`);
-    }
-    if(age<18 && age>0){
-        console.log(`your age is : ${age}, You can note vote`);
-    }
-}
-votingEligibility(20);
-votingEligibility(89);
-votingEligibility(1500);
-votingEligibility(0);
+
+let = num1= 1-2
+console.log(num1);
